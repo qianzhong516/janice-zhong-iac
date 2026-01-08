f21819bb0c5a520825da75f16913ff05f509de8f
qianzhong516 <ads.gutless810@passinbox.com>
2026-01-08 18:44:16 +1100

    remove reset record logic

--- dynamodb.tf
+++ dynamodb.tf
@@ -10,15 +10,3 @@ resource "aws_dynamodb_table" "visits" {
 
   tags = var.tags
 }
-
-resource "aws_dynamodb_table_item" "visit-items" {
-  table_name = aws_dynamodb_table.visits.name
-  hash_key   = aws_dynamodb_table.visits.hash_key
-
-  item = <<ITEM
-    {
-        "id": {"S": "1"},
-        "visitCount": {"N": "0"}
-    }
-  ITEM
-}
