d8907ed47773a2982227bb5532b02f0a1d7860d6
qianzhong516 <ads.gutless810@passinbox.com>
2026-01-09 15:01:41 +1100

    using hcp terraform backend now

--- main.tf
+++ main.tf
@@ -7,13 +7,6 @@ terraform {
       version = "6.27.0"
     }
   }
-
-  # TODO: iac the backend s3 bucket
-  backend "s3" {
-    bucket       = "janice-zhong-terraform"
-    key          = "terraform.state"
-    use_lockfile = true
-  }
 }
 
 provider "aws" {
