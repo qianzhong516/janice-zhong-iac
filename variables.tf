29fb64dd5fb32dbf6ee44058ada18377f1ecd219
qianzhong516 <ads.gutless810@passinbox.com>
2026-01-09 13:59:08 +1100

    remove default variable values

--- variables.tf
+++ variables.tf
@@ -1,13 +1,9 @@
 variable "bucket_name" {
   type = string
-  # TODO: delete this
-  default = "janice-zhong"
 }
 
 variable "domain_suffix" {
   type = string
-  # TODO: delete this
-  default = "com"
 }
 
 variable "tags" {
